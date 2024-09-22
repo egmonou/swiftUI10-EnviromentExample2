e1af975011137d15e85d5af6d5c7a7701efd30fe
Nour <mohamednour25@gmail.com>
2024-09-22 21:16:38 +0300

    Initial Commit

---
+++ EnviromentExample/EnviromentExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnviromentExampleApp.swift
+//  EnviromentExample
+//
+//  Created by administrator on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct EnviromentExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
